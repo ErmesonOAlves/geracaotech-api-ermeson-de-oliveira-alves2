be5f0a76ce17d121403dfed9e8cb415a41fff74c
ErmesonOAlves <ermesonoliveira2000@gmail.com>
2026-03-27 19:02:17 -0300

    feat:corrigido logica de atualizacao de usuario, agora cada usuario pode atualizar apenas seu proprio user

--- src/middlewares/authenticationMiddleware.js
+++ src/middlewares/authenticationMiddleware.js
@@ -1,8 +1,10 @@
 import 'dotenv/config'
 import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
 export const authenticationMiddleware = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
+
         if (!authHeader) {
             return res.status(401).json({
                 message: `Token must be provided`
@@ -19,6 +21,29 @@ export const authenticationMiddleware = (req, res, next) => {
         
         return res.status(401).json({
             message: "Invalid or expired token"
+        })
+    }
+}
+
+export const authorizationMiddleware =async (req,res,next)=>{
+    try {
+        const requestedId = parseInt(req.params.id);
+        const userId = req.user.id;
+        const userToUpdate = await User.findByPk(requestedId)
+        if(!userToUpdate){
+            return res.status(404).json({
+                message:'User not found'
+            })
+        }
+        if(userId !== requestedId){
+            return res.status(403).json({
+                message:'Access denied you can only update your own profile'
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            message:"Internal server error"
         })
     }
 }
